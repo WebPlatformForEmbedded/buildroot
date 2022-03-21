e9d16de6f1fc0e7729173602fab5b916f52ce28a
Lukasz Iwan <l.iwan@metrological.com>
2022-03-21 12:54:02 +0100

    Address PR comments

--- package/disney/disney-tooling/disney-tooling.mk
+++ package/disney/disney-tooling/disney-tooling.mk
@@ -11,72 +11,84 @@ DISNEY_TOOLING_LICENSE = PROPRIETARY
 DISNEY_TOOLING_INSTALL_STAGING = NO
 DISNEY_TOOLING_DEPENDENCIES = disney
 
-_DISNEY_EXTRACT_DIR = $(BUILD_DIR)/disney-$(DISNEY_VERSION)
+# Shields need to be built from the ADK sources.
+# A link is created from the shield-agent (this build dir) to the ADK build directory.
+# That way shield-agent and extension can be properly configured using configuration variables from the ADK
+# and a proper premake file can be selected. Then it is built using generated make files inside the ADK build directory.
+_DISNEY_BUILD_DIR = $(BUILD_DIR)/disney-$(DISNEY_VERSION)
+
+
+_DISNEY_TOOLING_CONFIGURE_FLAGS = --verbose \
+								  --file=shield-agent/premake5.lua \
+								  --target=$(_DISNEY_TARGET_NAME)
+
 
 ifeq ($(BR2_PACKAGE_DISNEY_PLAYER_UMA),y)
-_DISNEY_PLAYER = nve-shared
-_DISNEY_CURL_HTTP = --curl-http2
 DISNEY_TOOLING_DEPENDENCIES += disney-uma
+_DISNEY_TOOLING_CONFIGURE_FLAGS += --player=$(_DISNEY_PLAYER) $(_DISNEY_CURL_HTTP)
 endif
 
 define DISNEY_TOOLING_CONFIGURE_CMDS
-       ln -sf $(@D)/shield-agent ${_DISNEY_EXTRACT_DIR}
-       cd $(_DISNEY_EXTRACT_DIR) && CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" GCC_PREFIX="$(TARGET_CROSS)" PLATFORM="$(_DISNEY_TARGET_PLATFORM)" \
-          ARCH="$(KERNEL_ARCH)" SSL_VERSION="$(DISNEY_LIBOPENSSL_SO_VERSION)" SYSINCLUDEDIR="$(STAGING_DIR)/usr/include" \
-              SYSLIBDIR="$(STAGING_DIR)/usr/lib" \
-                 ./premake5 --verbose --file=shield-agent/premake5.lua --target=$(_DISNEY_TARGET_NAME) --player=$(_DISNEY_PLAYER) $(_DISNEY_CURL_HTTP) gmake2
+    ln -sf $(@D)/shield-agent ${_DISNEY_BUILD_DIR}
+    cd "$(_DISNEY_BUILD_DIR)" && \
+		CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" GCC_PREFIX="$(TARGET_CROSS)" PLATFORM="$(_DISNEY_TARGET_PLATFORM)" \
+    	ARCH="$(KERNEL_ARCH)" SSL_VERSION="$(DISNEY_LIBOPENSSL_SO_VERSION)" SYSINCLUDEDIR="$(STAGING_DIR)/usr/include" \
+    	SYSLIBDIR="$(STAGING_DIR)/usr/lib" \
+		./premake5 $(_DISNEY_TOOLING_CONFIGURE_FLAGS) gmake2
 endef
 
 ifeq ($(BR2_PACKAGE_DISNEY_TOOLING_SHIELD_AGENT),y)
 define _DISNEY_TOOLING_BUILD_SHIELD_AGENT
-       @echo ----------------------------------------------------------------------------
-       @echo Building Disney SHIELD Agent
-       @echo ----------------------------------------------------------------------------
-       cd $(_DISNEY_EXTRACT_DIR)/build && make shield-agent config=$(_DISNEY_BUILD_CONFIG) $(_DISNEY_VERBOSE)
+    @echo ----------------------------------------------------------------------------
+    @echo Building Disney SHIELD Agent
+    @echo ----------------------------------------------------------------------------
+    cd "$(_DISNEY_BUILD_DIR)/build" && \
+		make shield-agent config=$(_DISNEY_BUILD_CONFIG) $(_DISNEY_VERBOSE)
 endef
 endif
 
 ifeq ($(BR2_PACKAGE_DISNEY_TOOLING_SHIELD_EXTENSION),y)
 define _DISNEY_TOOLING_BUILD_SHIELD_EXTENSION
-       @echo ----------------------------------------------------------------------------
-       @echo Building Disney SHIELD Extension
-       @echo ----------------------------------------------------------------------------
-       cd $(_DISNEY_EXTRACT_DIR)/build && make shield-agent-extension morgana config=$(_DISNEY_BUILD_CONFIG) $(_DISNEY_VERBOSE)
+    @echo ----------------------------------------------------------------------------
+    @echo Building Disney SHIELD Extension
+    @echo ----------------------------------------------------------------------------
+    cd "$(_DISNEY_BUILD_DIR)/build" && \
+		make shield-agent-extension morgana config=$(_DISNEY_BUILD_CONFIG) $(_DISNEY_VERBOSE)
 endef
 endif
 
 define DISNEY_TOOLING_BUILD_CMDS
-       $(call _DISNEY_TOOLING_BUILD_SHIELD_AGENT)
-       $(call _DISNEY_TOOLING_BUILD_SHIELD_EXTENSION)
+	$(call _DISNEY_TOOLING_BUILD_SHIELD_AGENT)
+	$(call _DISNEY_TOOLING_BUILD_SHIELD_EXTENSION)
 endef
 
 
 ifeq ($(BR2_PACKAGE_DISNEY_TOOLING_SHIELD_AGENT),y)
 define _DISNEY_TOOLING_INSTALL_SHIELD_AGENT
-       @echo "Installing shield-agent executable"
-       $(INSTALL) -D -m 0755 $(_DISNEY_EXTRACT_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/shield-agent $(TARGET_DIR)/usr/bin/shield-agent
+    @echo "Installing shield-agent executable"
+    $(INSTALL) -D -m 0755 "$(_DISNEY_BUILD_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/shield-agent" "$(TARGET_DIR)/usr/bin/shield-agent"
 endef
 endif
 
 ifeq ($(BR2_PACKAGE_DISNEY_TOOLING_SHIELD_EXTENSION),y)
 define _DISNEY_TOOLING_INSTALL_SHIELD_EXTENSION
-       @echo "Installing Extensions"
-       cp -R $(_DISNEY_EXTRACT_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/extensions $(TARGET_DIR)$(_DISNEY_DATA_DIR)/shield_runtime/extensions
-       @echo "Installing morgana executable"
-       $(INSTALL) -D -m 0755 $(_DISNEY_EXTRACT_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/morgana $(TARGET_DIR)/usr/bin/morgana
+    @echo "Installing Extensions"
+    cp -R "$(_DISNEY_BUILD_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/extensions" "$(TARGET_DIR)$(_DISNEY_DATA_DIR)/shield_runtime/extensions"
+    @echo "Installing morgana executable"
+    $(INSTALL) -D -m 0755 "$(_DISNEY_BUILD_DIR)/build/bin/$(_DISNEY_TARGET_PLATFORM)/$(_DISNEY_BUILD_TYPE)/morgana" "$(TARGET_DIR)/usr/bin/morgana"
 endef
 endif
 
 define _DISNEY_TOOLING_INSTALL
-       @echo "Installing shield_runtime"
-       cp -R $(_DISNEY_EXTRACT_DIR)/shield_runtime $(TARGET_DIR)$(_DISNEY_DATA_DIR)
-       cp -R $(_DISNEY_EXTRACT_DIR)/build/shield_runtime/shield_agent_data/assets/dy_lib_tests  $(TARGET_DIR)$(_DISNEY_DATA_DIR)/shield_runtime/shield_agent_data/assets
-       $(call _DISNEY_TOOLING_INSTALL_SHIELD_AGENT)
-       $(call _DISNEY_TOOLING_INSTALL_SHIELD_EXTENSION)
+    @echo "Installing shield_runtime"
+    cp -R "$(_DISNEY_BUILD_DIR)/shield_runtime" "$(TARGET_DIR)$(_DISNEY_DATA_DIR)"
+    cp -R "$(_DISNEY_BUILD_DIR)/build/shield_runtime/shield_agent_data/assets/dy_lib_tests" "$(TARGET_DIR)$(_DISNEY_DATA_DIR)/shield_runtime/shield_agent_data/assets"
+    $(call _DISNEY_TOOLING_INSTALL_SHIELD_AGENT)
+    $(call _DISNEY_TOOLING_INSTALL_SHIELD_EXTENSION)
 endef
 
 define DISNEY_TOOLING_INSTALL_TARGET_CMDS
-       $(call _DISNEY_TOOLING_INSTALL)
+    $(call _DISNEY_TOOLING_INSTALL)
 endef
 
 
