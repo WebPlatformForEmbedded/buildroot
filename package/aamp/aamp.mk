a3605d8d0640f9a890854746fa873f1e17349cf8
Haseena Sainul <h.sainul@metrological.com>
2022-05-19 05:49:53 -0700

    Aamp: add wpeframework-clientlibraries into the dependency list i n OCDM enabled case

--- package/aamp/aamp.mk
+++ package/aamp/aamp.mk
@@ -12,6 +12,7 @@ AAMP_INSTALL_STAGING = YES
 AAMP_DEPENDENCIES = libcurl libdash libxml2 aampabr aampmetrics gst1-plugins-base wpeframework
 
 ifeq ($(BR2_PACKAGE_WPEFRAMEWORK_CDMI), y)
+AAMP_DEPENDENCIES += wpeframework-clientlibraries
 AAMP_CONF_OPTS += \
         -DENABLE_SESSION_STATS=ON \
 	-DCMAKE_DASH_DRM=ON \
