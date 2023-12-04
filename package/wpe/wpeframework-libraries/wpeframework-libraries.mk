025728cf031614ee6c90a43f186ed45e993b9fd4
Volkan Aslan <83580341+volkan-aslan@users.noreply.github.com>
2023-12-04 17:00:46 +0100

    Update wpeframework-libraries.mk

--- package/wpe/wpeframework-libraries/wpeframework-libraries.mk
+++ package/wpe/wpeframework-libraries/wpeframework-libraries.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 WPEFRAMEWORK_LIBRARIES_VERSION = 1c3dd1677c8c50e9582e5e0027fdf5a2c2b79abb
-WPEFRAMEWORK_LIBRARIES_SITE_METHOD = git
-WPEFRAMEWORK_LIBRARIES_SITE = git@github.com:WebPlatformForEmbedded/ThunderLibraries.git
+WPEFRAMEWORK_LIBRARIES_SITE = $(call github,WebPlatformForEmbedded,ThunderLibraries,$(WPEFRAMEWORK_LIBRARIES_VERSION))
 WPEFRAMEWORK_LIBRARIES_INSTALL_STAGING = YES
 WPEFRAMEWORK_LIBRARIES_DEPENDENCIES = wpeframework
 
