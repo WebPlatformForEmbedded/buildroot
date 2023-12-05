5415040319e1063756915b26dd47f110867e149e
Volkan Aslan <v.aslan@metrological.com>
2023-12-05 22:54:15 +0100

    Updating OCDM-Clearkey git setting in mk file

--- package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
+++ package/wpe/wpeframework-cdmi/wpeframework-cdmi-clearkey/wpeframework-cdmi-clearkey.mk
@@ -4,8 +4,7 @@
 #
 ################################################################################
 WPEFRAMEWORK_CDMI_CLEARKEY_VERSION = R4.4.1
-WPEFRAMEWORK_CDMI_CLEARKEY_SITE_METHOD = git
-WPEFRAMEWORK_CDMI_CLEARKEY_SITE = git@github.com:rdkcentral/OCDM-Clearkey.git
+WPEFRAMEWORK_CDMI_CLEARKEY_SITE = $(call github,rdkcentral,OCDM-Clearkey,$(WPEFRAMEWORK_CDMI_CLEARKEY_VERSION))
 WPEFRAMEWORK_CDMI_CLEARKEY_INSTALL_STAGING = YES
 WPEFRAMEWORK_CDMI_CLEARKEY_DEPENDENCIES = wpeframework-clientlibraries libopenssl
 
