41f1fabdf90e821d73f3bfa2e6cb48f9c63cb3be
Nina Kolesa <nina@igalia.com>
2025-02-18 17:26:47 +0100

    wpewebkit: add LTO build option

--- package/pkg-cmake.mk
+++ package/pkg-cmake.mk
@@ -267,6 +267,7 @@ define TOOLCHAIN_CMAKE_INSTALL_FILES
 		-e 's#@@TOOLCHAIN_HAS_CXX@@#$(if $(BR2_INSTALL_LIBSTDCPP),1,0)#' \
 		-e 's#@@TOOLCHAIN_HAS_FORTRAN@@#$(if $(BR2_TOOLCHAIN_HAS_FORTRAN),1,0)#' \
 		-e 's#@@CMAKE_BUILD_TYPE@@#$(if $(BR2_ENABLE_RUNTIME_DEBUG),Debug,Release)#' \
+		-e 's#@@GNU_TARGET_NAME@@#$(call qstrip,$(GNU_TARGET_NAME))#' \
 		$(TOPDIR)/support/misc/toolchainfile.cmake.in \
 		> $(HOST_DIR)/share/buildroot/toolchainfile.cmake
 	$(Q)$(INSTALL) -D -m 0644 support/misc/Buildroot.cmake \
