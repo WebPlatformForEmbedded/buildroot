3f2d2a87956e2677f96ab091e9f5c6b09173532e
Volkan <volkan.aslan@outlook.com>
2026-08-12 23:00:55 +0200

    R4.4.7 hash updates

--- package/wpe/wpeframework/wpeframework.mk
+++ package/wpe/wpeframework/wpeframework.mk
@@ -3,7 +3,7 @@
 # wpeframework
 #
 ################################################################################
-WPEFRAMEWORK_VERSION = R4.4.6
+WPEFRAMEWORK_VERSION = R4.4.7
 WPEFRAMEWORK_SITE = $(call github,rdkcentral,Thunder,$(WPEFRAMEWORK_VERSION))
 WPEFRAMEWORK_INSTALL_STAGING = YES
 WPEFRAMEWORK_DEPENDENCIES = zlib $(call qstrip,$(BR2_PACKAGE_SDK_INSTALL)) host-wpeframework-tools
